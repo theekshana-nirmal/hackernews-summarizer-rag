e16ba42957c91159fb7c21c604ac99bc6a93f4a8
Theekshana Nirmal <131228387+theekshana-nirmal@users.noreply.github.com>
2026-03-28 01:26:00 +0530

    feat: implement main function to load web page, split content into chunks, create embeddings, and initialize vector store

--- index.ts
+++ index.ts
@@ -7,3 +7,36 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { createRetrievalChain } from "@langchain/classic/chains/retrieval";
 import { createStuffDocumentsChain } from "@langchain/classic/chains/combine_documents";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
+
+
+async function main() {
+    // STEP 01: Load the Web Page
+    const loader = new CheerioWebBaseLoader("https://news.ycombinator.com/", {selector: ".titleline"});
+    const docs = await loader.load();
+
+    console.log(docs[0].pageContent);
+
+    // STEP 02: Split Into Chunks
+    const splitter = new RecursiveCharacterTextSplitter({
+        chunkSize: 150,
+        chunkOverlap: 15
+    });
+
+    const chunks = await splitter.splitDocuments(docs);
+    console.log(chunks);
+
+    // STEP 03: Create Embeddings and Store in the Vector Store
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        modelName: "gemini-embedding-001"
+    });
+
+    const vectorStore = await MemoryVectorStore.fromDocuments(chunks, embeddings);
+
+    console.log("Vector store created and loaded with chunks.");
+    console.log(vectorStore);
+
+    // STEP 04: Build the Retrieval Chain
+    
+}
+
+main();
